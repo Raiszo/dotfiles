9c88d7c57a209062fb2993da19cebddb40078fb6
Raiszo <E05.sek@gmail.com>
2019-04-19 11:02:55 -0500

    :dagger: almost everything is done with use-package

--- emacs/init.el
+++ emacs/init.el
@@ -9,7 +9,7 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 													("melpa" . "http://melpa.milkbox.net/packages/")) )
 
-(setq package-list '(js2-mode undo-tree company-tern company ace-window neotree multiple-cursors multi-term monokai-theme powerline magit highlight-indent-guides ob-mongo zoom-window nyan-mode farmhouse-theme yasnippet zoom-window emojify org-bullets org-trello all-the-icons expand-region telephone-line markdown-mode darkokai-theme phi-search nodejs-repl exec-path-from-shell ob-sql-mode elpy use-package projectile persp-projectile dockerfile-mode nginx-mode yaml-mode))
+(setq package-list '(js2-mode monokai-theme ob-mongo farmhouse-theme org-trello all-the-icons markdown-mode darkokai-theme phi-search nodejs-repl exec-path-from-shell ob-sql-mode elpy use-package projectile persp-projectile dockerfile-mode nginx-mode yaml-mode))
 
 ;; macos only stuff >:v, pice of crap
 (when (memq window-system '(mac ns x))
@@ -29,7 +29,7 @@
 ;; (global-set-key [tab] 'tab-to-tab-stop) ; same as Ctrl+i
 
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/intento1.el")
+;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/intento1.el")
 ;;(load-theme 'monokai-theme)
 (add-hook 'after-init-hook (lambda () (load-theme 'darkokai)))
 (setq darkokai-mode-line-padding 1)
@@ -41,7 +41,6 @@
 
 (add-hook 'window-setup-hook 'on-after-init)
 
-;;Custom tab length
 ;; (setq-default tab-width 2)
 (add-hook 'after-init-hook 'global-company-mode)
 
@@ -50,19 +49,6 @@
 ;; make return key also do indent, globally
 (electric-indent-mode 1)
 
-;;(global-set-key "\M-[1;5C"    'forward-word)      ; Ctrl+right   => forward word
-;;(global-set-key "\M-[1;5D"    'backward-word)     ; Ctrl+left    => backward word
-
-;;(require 'auto-complete)
-;;(require 'auto-complete-config)
-;;(ac-config-default)
-;;(add-hook 'js2-mode-hook 'ac-js2-mode)
-
-
-
-;; js-mode
-;; (setq js-indent-level 2)
-
 ;; js2-mode
 (setq js2-strict-missing-semi-warning nil)
 (setq js2-include-node-externs t)
@@ -70,13 +56,13 @@
 ;; LSP mode config
 (use-package lsp-mode
   :commands lsp
-	:init
-	(setq lsp-enable-indentation nil)
-	(setq lsp-prefer-flymake nil)
-	)
+  :init
+  (setq lsp-enable-indentation nil)
+  (setq lsp-prefer-flymake nil)
+  )
 (add-hook 'python-mode-hook #'lsp)
-(require 'company-lsp)
-(push 'company-lsp company-backends)
+;; (require 'company-lsp)
+;; (push 'company-lsp company-backends)
 
 ;; js-mode
 ;; (setq js-indent-level 2)
@@ -86,45 +72,61 @@
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 (setq-default js2-basic-offset 4)
 
-(require 'undo-tree)
-(global-undo-tree-mode)
+(use-package undo-tree
+  :ensure t
+  :config
+  (global-undo-tree-mode 1)
+  )
 
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-;; (setq neo-smart-open t)
+(use-package all-the-icons
+  :ensure t
+  )
 
-(require 'all-the-icons)
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-c C-v" . 'mc/edit-lines)
+	 ("C-<" . 'mc/mark-next-like-this)
+	 ("C->" . mc/mark-previous-like-this)
+	 ("C-c C-q" . mc/mark-all-like-this))
+  )
 
-(require 'multiple-cursors)
-(global-set-key (kbd "C-c C-v") 'mc/edit-lines)
-(global-set-key (kbd "C-<") 'mc/mark-next-like-this)
-(global-set-key (kbd "C->") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-q") 'mc/mark-all-like-this)
+(use-package ace-window
+  :ensure t
+  :bind ("M-o" . ace-window)
+  )
 
-(require 'ace-window)
-(global-set-key (kbd "M-o") 'ace-window)
-(require 'zoom-window)
-(global-set-key (kbd "C-x 4") 'zoom-window-zoom)
-(setq zoom-window-mode-line-color "DarkViolet")
+(use-package zoom-window
+  :ensure t
+  :bind ("C-x 4" . zoom-window-zoom)
+  :custom
+  (zoom-window-mode-line-color "DarkViolet" "Distinctive color when using zoom")
+  )
 
+(use-package nyan-mode
+  :ensure t
+  :config
+  (nyan-mode 1)
+  (nyan-start-animation)
+  (nyan-toggle-wavy-trail)
+  )
 
-(require 'nyan-mode)
-(nyan-mode t)
-(nyan-start-animation)
-(nyan-toggle-wavy-trail)
+(use-package highlight-indent-guides
+  :ensure t
+  :hook (prog-mode . highlight-indent-guides-mode)
+  :config
+  (setq highlight-indent-guides-method 'character)
+  (highlight-indent-guides-mode 1)
+  )
 
-(require 'highlight-indent-guides)
-(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
-(setq highlight-indent-guides-method 'character)
+(use-package multi-term
+  :ensure t
+  :config
+  ;; want to use Ace-window here, so delete it from the alist
+  (delete* "M-o" term-bind-key-alist :test 'equal :key 'car)
+  ;; No need to add-to-list, just to be clear with the new functionality :D
+  (add-to-list 'term-bind-key-alist '("M-o" . ace-window))
+  )
 
-(require 'multi-term)
-;; want to use Ace-window here, so delete it from the alist
-(delete* "M-o" term-bind-key-alist :test 'equal :key 'car)
-;; (delete* "M-^?" term-bind-key-alist :test 'equal :key 'car)
-;; No need to add-to-list, just to be clear with the new functionality :D
-(add-to-list 'term-bind-key-alist '("M-o" . ace-window))
-;; (assoc "M-o" term-bind-key-alist)
 
 (when (version<= "26.0.50" emacs-version )
   (add-hook 'prog-mode-hook #'display-line-numbers-mode)
@@ -135,21 +137,26 @@
 (show-paren-mode)
 (electric-pair-mode)
 (ido-mode t)
-;; (setq web-mode-enable-auto-closing t)
 (global-hl-line-mode +1)
 
 
 ;; Setting transparency, not working like urxvt
-(set-frame-parameter (selected-frame) 'alpha '(85 85))
-(add-to-list 'default-frame-alist '(alpha 85 85))
+;; (set-frame-parameter (selected-frame) 'alpha '(85 85))
+;; (add-to-list 'default-frame-alist '(alpha 85 85))
 
-(require 'yasnippet)
-(yas-load-directory "~/.emacs.d/snippets")
-(yas-reload-all)
-(add-hook 'js2-mode-hook #'yas-minor-mode)
+(use-package yasnippet
+  :ensure t
+  :hook (js2-mode . yas-minor-mode)
+  :config
+  (yas-load-directory "~/.emacs.d/snippets")
+  (yas-reload-all)
+  )
 
-(require 'org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(use-package org-bullets
+  :ensure t
+  :hook (org-mode . (lambda () (org-bullets-mode 1)))
+  )
+
 
 
 (org-babel-do-load-languages
@@ -182,74 +189,85 @@
 (set-face-attribute 'org-block-begin-line nil :background "black" :foreground "green")
 (set-face-attribute 'org-block-end-line nil :background "black" :foreground "green")
 
-(require 'org-trello)
-(setq org-trello-files (directory-files "~/Documents/trello" t "\\.org$"))
+;; (require 'org-trello)
+;; (setq org-trello-files (directory-files "~/Documents/trello" t "\\.org$"))
 
 
-(require 'emojify)
-(add-hook 'after-init-hook #'global-emojify-mode)
-(setq emojify-user-emojis '((":trollface:" . (("name" . "Troll Face")
-																							("image" . "~/.emacs.d/emojis/custom/trollface.png")
-                                              ("style" . "github")))
-														(":kappa:" . (("name". "Kappa")
-																					("image" . "~/.emacs.d/emojis/custom/kappa.png")
-																					("style" . "github")))
-														))
-;; If emojify is already loaded refresh emoji data
-(when (featurep 'emojify)
-  (emojify-set-emoji-data))
-(emojify-mode-line-mode)
+(use-package emojify
+  :ensure t
+  :hook (after-init . global-emojify-mode)
+  :config
+  (setq emojify-user-emojis
+	'((":trollface:" . (("name" . "Troll Face")
+			    ("image" . "~/.emacs.d/emojis/custom/trollface.png")
+			    ("style" . "github")))
+	  (":kappa:" . (("name". "Kappa")
+			("image" . "~/.emacs.d/emojis/custom/kappa.png")
+			("style" . "github")))
+	  ))
+  (when (featurep 'emojify)
+    (emojify-set-emoji-data))
+  (emojify-mode-line-mode 1)
+  )
 
 ;; Use DejaVu Sans Mono Nerd Font
 
-(set-face-attribute 'default nil :font "DejaVuSansMono Nerd Font Mono")
-(set-face-attribute 'default nil :height 135)
+;; (set-face-attribute 'default nil :font "DejaVuSansMono Nerd Font Mono")
+;; (set-face-attribute 'default nil :height 135)
 ;; (require 'powerline)
 ;; (powerline-default-theme)
 ;; (setq powerline-default-separator 'wave)
 
-(require 'expand-region)
-(if (memq window-system '(mac ns x))
-		(global-set-key (kbd "C-=") 'er/expand-region)
-	(global-set-key (kbd "C-¿") 'er/expand-region))
+(use-package expand-region
+  :ensure t
+  :bind ("C-=" . 'er/expand-region)
+  )
 
-
-
-(require 'telephone-line)
-(defface my-indianRed '((t (:foreground "white" :background "IndianRed1"))) "")
-(defface my-gold '((t (:foreground "black" :background "gold"))) "")
-
+(use-package telephone-line
+  :ensure t
+  :config
+  (defface my-indianRed '((t (:foreground "white" :background "IndianRed1"))) "")
+  (defface my-gold '((t (:foreground "black" :background "gold"))) "")
+  (setq telephone-line-faces
+  	'((indianGold . (my-gold . my-indianRed))
+  	  (accent . (telephone-line-accent-active . telephone-line-accent-inactive))
+  	  (nil . (mode-line . mode-line-inactive))))
+  (setq telephone-line-lhs
+  	'((indianGold . (telephone-line-vc-segment
+			 telephone-line-erc-modified-channels-segment
+			 telephone-line-process-segment))
+  	  (nil . (telephone-line-major-mode-segment
+  		  telephone-line-buffer-segment))
+	  ;; when splitting the window it gets trimmed to 1 ;'v
+	  ;; refer to this issue https://github.com/dbordak/telephone-line/issues/41
+	  (nil . (telephone-line-nyan-segment))
+	  ))
+  (setq telephone-line-rhs
+  	'((nil . (telephone-line-misc-info-segment))
+  	  (accent . (telephone-line-minor-mode-segment))
+  	  (indianGold . (telephone-line-airline-position-segment))
+	  ))
+  (telephone-line-mode 1)
+  )
 ;; this not working yet D:
-(telephone-line-defsegment* s1 ()
-	":dagger:")
+;; (telephone-line-defsegment* s1 ()
+;; 	":dagger:")
 
-(setq telephone-line-faces
-			'((indianGold . (my-gold . my-indianRed))
-				(accent . (telephone-line-accent-active . telephone-line-accent-inactive))
-				(nil . (mode-line . mode-line-inactive))))
-(setq telephone-line-lhs
-      '((indianGold . (telephone-line-process-segment
-											telephone-line-vc-segment
-											telephone-line-erc-modified-channels-segment))
-        (nil			. (telephone-line-buffer-segment
-										 telephone-line-major-mode-segment
-										 telephone-line-nyan-segment))))
-(setq telephone-line-rhs
-      '((nil    . (telephone-line-misc-info-segment))
-        (accent . (telephone-line-minor-mode-segment))
-        (indianGold   . (telephone-line-airline-position-segment))))
-(telephone-line-mode 1)
+(use-package magit
+  :ensure t
+  :bind ("<f5>" . magit-status)
+  )
 
-;; Magit
-(global-set-key [f5] 'magit-status)
 ;; ibuffer, I like to kill buffers :)
 (global-set-key [f6] 'ibuffer)
 
 
+(use-package phi-search
+  :ensure t
+  :bind (("C-s" . phi-search)
+	 ("C-r" . phi-search-backward))
+  )
 
-(require 'phi-search)
-(global-set-key (kbd "C-s") 'phi-search)
-(global-set-key (kbd "C-r") 'phi-search-backward)
 
 ;; Python
 (elpy-enable)
@@ -265,23 +283,23 @@
 (require 'persp-projectile)
 (define-key projectile-mode-map (kbd "C-c p") 'persp-switch)
 
-(defun my-web-mode-hook ()
-  (setq web-mode-markup-indent-offset 2)
-  (web-mode-use-tabs)
-  (setq-default tab-width 4)
-)
-(use-package web-mode
-  :mode (("\\.html$" . web-mode)
-	 ("\\.ejs$" . web-mode))
-  :init
-  (add-hook 'web-mode-hook  'my-web-mode-hook)
-  )
+;; (defun my-web-mode-hook ()
+;;   (setq web-mode-markup-indent-offset 2)
+;;   (web-mode-use-tabs)
+;;   (setq-default tab-width 4)
+;; )
+;; (use-package web-mode
+;;   :mode (("\\.html$" . web-mode)
+;; 	 ("\\.ejs$" . web-mode))
+;;   :init
+;;   (add-hook 'web-mode-hook  'my-web-mode-hook)
+;;   )
 
-(use-package emmet-mode
-  :config
-  (add-hook 'sgml-mode-hook 'emmet-mode)
-  (add-hook 'web-mode-hook 'emmet-mode)
-  )
+;; (use-package emmet-mode
+;;   :config
+;;   (add-hook 'sgml-mode-hook 'emmet-mode)
+;;   (add-hook 'web-mode-hook 'emmet-mode)
+;;   )
 
 (use-package restclient-mode
   :mode (("\\.http$" . restclient-mode))
