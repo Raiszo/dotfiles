7d4579b5145d14d5ebe71d4c535a75452fd7d759
Raiszo <E05.sek@gmail.com>
2020-05-21 09:32:03 -0500

    :dagger: updating packages + setting up for doom-modeline

--- emacs/init.el
+++ emacs/init.el
@@ -4,9 +4,10 @@
 (require 'package)
 ;;Add melpa repository
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("melpa" . "http://melpa.milkbox.net/packages/")) )
+			 ("melpa-stable" . "http://stable.melpa.org/packages/")
+			 ("melpa" . "http://melpa.org/packages/")) )
 
-(setq package-list '(js2-mode monokai-theme ob-mongo farmhouse-theme org-trello all-the-icons markdown-mode darkokai-theme phi-search nodejs-repl exec-path-from-shell ob-sql-mode elpy use-package dockerfile-mode nginx-mode yaml-mode))
+(setq package-list '(js2-mode monokai-theme farmhouse-theme darkokai-theme exec-path-from-shell use-package nginx-mode))
 
 (dolist (package package-list)
   (unless (package-installed-p package)
@@ -39,7 +40,7 @@
   (setq doom-themes-enable-italic t)
   :config
   (progn
-    (doom-themes-treemacs-config)
+    ;; (doom-themes-treemacs-config)
     (doom-themes-org-config)))
 
 (defun on-after-init ()
@@ -64,43 +65,34 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 (setq-default js2-basic-offset 4)
-(setq-default js-switch-indent-offset 4)
+
+(use-package nodejs-repl
+  :ensure t)
 
 (use-package undo-tree
   :ensure t
   :config
-  (global-undo-tree-mode 1)
-  )
+  (global-undo-tree-mode 1))
 
 (use-package all-the-icons
-  :ensure t
-  )
+  :ensure t)
 
 (use-package multiple-cursors
   :ensure t
   :bind (("C-c C-v" . 'mc/edit-lines)
 	 ("C-<" . 'mc/mark-next-like-this)
 	 ("C->" . mc/mark-previous-like-this)
-	 ("C-c C-q" . mc/mark-all-like-this))
-  )
+	 ("C-c C-q" . mc/mark-all-like-this)))
 
 (use-package ace-window
   :ensure t
-  :bind ("M-o" . ace-window)
-  )
+  :bind ("M-o" . ace-window))
 
 (use-package zoom-window
   :ensure t
   :bind ("C-x 4" . zoom-window-zoom)
   :custom
   (zoom-window-mode-line-color "DarkViolet" "Distinctive color when using zoom"))
-
-(use-package nyan-mode
-  :ensure t
-  :config
-  (nyan-mode 1)
-  (nyan-start-animation)
-  (nyan-toggle-wavy-trail))
 
 (use-package highlight-indent-guides
   :ensure t
@@ -113,7 +105,7 @@
   :ensure t
   :config
   ;; want to use Ace-window here, so delete it from the alist
-  (cl-delete "M-o" term-bind-key-alist :test 'equal :key 'car)
+  (delete* "M-o" term-bind-key-alist :test 'equal :key 'car)
   ;; No need to add-to-list, just to be clear with the new functionality :D
   (add-to-list 'term-bind-key-alist '("M-o" . ace-window)))
 
@@ -147,17 +139,13 @@
   :hook (org-mode . (lambda () (org-bullets-mode 1))))
 
 
-
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((lisp . t)
-   (C . t)
-   (emacs-lisp . t)
-   (latex . t)
-   (mongo . t)
-   (sql . t)))
+	 (C . t)
+	 (emacs-lisp . t)
+	 (latex . t)))
 (setq org-confirm-babel-evaluate nil)
-(require 'ob-js)
 
 ;; more space in GUI mode :D
 (setq inhibit-startup-screen t)
@@ -178,10 +166,6 @@
 (set-face-attribute 'org-block-begin-line nil :background "black" :foreground "green")
 (set-face-attribute 'org-block-end-line nil :background "black" :foreground "green")
 
-;; (require 'org-trello)
-;; (setq org-trello-files (directory-files "~/Documents/trello" t "\\.org$"))
-
-
 (use-package emojify
   :ensure t
   :hook (after-init . global-emojify-mode)
@@ -196,53 +180,50 @@
 	  ))
   (when (featurep 'emojify)
     (emojify-set-emoji-data))
-  (emojify-mode-line-mode 1)
-  )
+  (emojify-mode-line-mode 1))
 
 ;; Use DejaVu Sans Mono Nerd Font
 
-(set-face-attribute 'default nil :font "DejaVuSansMono Nerd Font Mono")
-(set-face-attribute 'default nil :height 135)
+;; (set-face-attribute 'default nil :font "DejaVuSansMono Nerd Font Mono")
+;; (set-face-attribute 'default nil :height 135)
 
 (use-package expand-region
   :ensure t
-  :bind ("C-=" . 'er/expand-region)
-  )
+  :bind ("C-=" . 'er/expand-region))
 
-(use-package telephone-line
-  :ensure t
-  :config
-  (defface my-indianRed '((t (:foreground "white" :background "IndianRed1"))) "")
-  (defface my-gold '((t (:foreground "black" :background "gold"))) "")
-  (setq telephone-line-faces
-	'((indianGold . (my-gold . my-indianRed))
-	  (accent . (telephone-line-accent-active . telephone-line-accent-inactive))
-	  (nil . (mode-line . mode-line-inactive))))
-  (setq telephone-line-lhs
-	'((indianGold . (telephone-line-vc-segment
-			 telephone-line-erc-modified-channels-segment
-			 telephone-line-process-segment))
-	  (nil . (telephone-line-major-mode-segment
-		  telephone-line-buffer-segment))
-	  ;; when splitting the window it gets trimmed to 1 ;'v
-	  ;; refer to this issue https://github.com/dbordak/telephone-line/issues/41
-	  (nil . (telephone-line-nyan-segment))
-	  ))
-  (setq telephone-line-rhs
-	'((nil . (telephone-line-misc-info-segment))
-	  (accent . (telephone-line-minor-mode-segment))
-	  (indianGold . (telephone-line-airline-position-segment))
-	  ))
-  (telephone-line-mode 1)
-  )
+;; (use-package telephone-line
+;;   :ensure t
+;;   :config
+;;   (defface my-indianRed '((t (:foreground "white" :background "IndianRed1"))) "")
+;;   (defface my-gold '((t (:foreground "black" :background "gold"))) "")
+;;   (setq telephone-line-faces
+;; 	'((indianGold . (my-gold . my-indianRed))
+;; 	  (accent . (telephone-line-accent-active . telephone-line-accent-inactive))
+;; 	  (nil . (mode-line . mode-line-inactive))))
+;;   (setq telephone-line-lhs
+;; 	'((indianGold . (telephone-line-vc-segment
+;; 			 telephone-line-erc-modified-channels-segment
+;; 			 telephone-line-process-segment))
+;; 	  (nil . (telephone-line-major-mode-segment
+;; 		  telephone-line-buffer-segment))
+;; 	  ;; when splitting the window it gets trimmed to 1 ;'v
+;; 	  ;; refer to this issue https://github.com/dbordak/telephone-line/issues/41
+;; 	  (nil . (telephone-line-nyan-segment))
+;; 	  ))
+;;   (setq telephone-line-rhs
+;; 	'((nil . (telephone-line-misc-info-segment))
+;; 	  (accent . (telephone-line-minor-mode-segment))
+;; 	  (indianGold . (telephone-line-airline-position-segment))
+;; 	  ))
+;;   (telephone-line-mode 1))
+
 ;; this not working yet D:
 ;; (telephone-line-defsegment* s1 ()
 ;;	":dagger:")
 
 (use-package magit
   :ensure t
-  :bind ("<f5>" . magit-status)
-  )
+  :bind ("<f5>" . magit-status))
 
 ;; ibuffer, I like to kill buffers :)
 (global-set-key [f6] 'ibuffer)
@@ -253,23 +234,49 @@
   :bind (("C-s" . phi-search)
 	 ("C-r" . phi-search-backward)))
 
-(use-package persp-mode
-  :ensure persp-projectile
-  :init
-  (persp-mode)
-  :bind (("M-s" . projectile-persp-switch-project)
-	 ("C-c p" . 'persp-switch)))
+;; (use-package helm
+;;   :ensure t
+;;   :pin melpa-stable
+;;   ;; when using helm-M-x emacs breaks (14% CPU)
+;;   ;; gonna wait for WSL 2.0, maybe it helps :'v
+;;   ;; :bind (("M-x" . helm-M-x)
+;;   ;; 	 ("C-x C-f" . helm-find-files))
+;;   :bind (("C-x C-f" . helm-find-files))
+;;   :config
+;;   (bind-keys :map helm-map
+;; 	     ("TAB" . helm-execute-persistent-action))
+;;   (setq helm-split-window-in-side-p t)
+;;   ;; (use-package helm-projectile
+;;   ;;   :ensure t
+;;   ;;   :config
+;;   ;;   (helm-projectile-on))
+;;   (helm-autoresize-mode 1)
+;;   (helm-mode 1))
 
 (use-package projectile
   :ensure t
   :config
-  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  ;; (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (projectile-mode +1))
+  ;; (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map))
+  ;; (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+;; (use-package perspective
+;;   :ensure t
+;;   :config
+;;   (persp-mode))
+
+;; (use-package persp-projectile
+;;   :ensure t)
+  ;; :config
+  ;; (define-key projectile-mode-map (kbd "s-s") 'projectile-persp-switch-project))
 
 ;; (use-package persp-mode
-;;   :ensure t)
-
+;;   :ensure persp-projectile
+;;   :init
+;;   (persp-mode)
+;;   :bind (("M-s" . projectile-persp-switch-project)
+;; 	 ("C-c p" . 'persp-switch)))
 
 (use-package treemacs
   :ensure t
@@ -283,59 +290,20 @@
     (setq treemacs-width 25)))
 
 (use-package treemacs-projectile
-  :after treemacs projectile
-  :ensure t)
+  :ensure t
+  :after treemacs projectile)
 
 (use-package treemacs-icons-dired
   :after treemacs dired
   :ensure t
   :config (treemacs-icons-dired-mode))
 
+;; (use-package treemacs-icons
+;;   :after treemacs
+;;   :ensure t)
+
 (use-package treemacs-magit
   :after treemacs magit
-  :ensure t)
-
-(use-package treemacs-icons
-  :after treemacs )
-
-(use-package helm
-  :ensure t
-  :bind (("M-x" . helm-M-x)
-	 ("C-x C-f" . helm-find-files))
-  :config
-  (setq helm-split-window-in-side-p t)
-  (bind-keys :map helm-map
-             ("TAB" . helm-execute-persistent-action))
-  ;; (use-package helm-projectile
-  ;;   :ensure t
-  ;;   :config
-  ;;   (helm-projectile-on))
-  (helm-autoresize-mode 1)
-  (helm-mode 1))
-
-(use-package go-mode
-  :ensure t
-  :custom (gofmt-command "goimports")
-  :config
-  (add-hook 'before-save-hook #'gofmt-before-save)
-  (use-package gotest
-    :ensure t)
-  (use-package go-tag
-    :ensure t
-    :config (setq go-tag-args (list "-transform"))))
-
-(use-package elixir-mode
-  :ensure t)
-
-(use-package yaml-mode
-  :ensure t
-  :mode ("\\.yaml\\'" "\\.yml\\'" "\\.config\\'")
-  :config
-  (setq yaml-indent-offset 4)
-  :custom-face
-  (font-lock-variable-name-face ((t (:foreground "violet")))))
-
-(use-package json-mode
   :ensure t)
 
 ;; (use-package origami
@@ -355,19 +323,31 @@
 ;;   (setq pipenv-with-flycheck nil)
 ;;   )
 
+;; posframe
+;; (use-package posframe
+;;   :ensure t)
+
+;; LSP mode config
+(use-package lsp-mode
+  :ensure t
+  :commands lsp
+  :config
+  (setq lsp-enable-indentation nil)
+  (setq lsp-prefer-flymake nil)
+  (setq lsp-auto-guess-root t))
+
+  ;; :init
+  ;; (add-hook 'lsp-mode-hook
+  ;;           (lambda ()
+  ;;             (when (featurep 'perspective)
+  ;;               (ad-deactivate 'set-window-buffer)))))
+
 (use-package lsp-ui
   :ensure t
   :commands lsp-ui-mode
-  :hook (lsp-mode . lsp-ui-mode)
   :config
   (setq lsp-ui-sideline-ignore-duplicate t)
   (setq lsp-ui-sideline-enable nil))
-(use-package company-lsp
-  :ensure t
-  :commands company-lsp
-  :config
-  (setq company-dabbrev-downcase 0)
-  (setq company-idle-delay 0))
 
 (use-package company
   :ensure t
@@ -376,38 +356,31 @@
   :config
   (progn
     (setq company-tooltip-align-annotations t
-	  ;; Easy navigation to candidates with M-<n>
-	  company-show-numbers t)
+          ;; Easy navigation to candidates with M-<n>
+          company-show-numbers t)
     (setq company-dabbrev-downcase nil))
+  :custom
+  (company-idle-delay 0)
+  (company-echo-delay 0)
+  (company-minimum-prefix-length 1)
   :diminish company-mode)
 (use-package company-quickhelp          ; Documentation popups for Company
   :ensure t
   :defer t
   :init (add-hook 'global-company-mode-hook #'company-quickhelp-mode))
-
-;; LSP mode config
-(use-package lsp
-  :ensure lsp-mode
-  :commands lsp
-  :hook ((js2-mode . lsp)
-	 (go-mode . lsp)
-	 (dockerfile-mode . lsp)
-	 (yaml-mode . lsp))
-  :config
-  (setq lsp-enable-indentation nil)
-  (setq lsp-prefer-flymake nil)
-  (setq lsp-auto-guess-root t)
-  :init
-  (add-hook 'lsp-mode-hook
-            (lambda ()
-              (when (featurep 'perspective)
-                (ad-deactivate 'set-window-buffer)))))
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp)
+;; (use-package company-box
+;;   :ensure t
+;;   :hook (company-mode . company-box-mode))
 
 (use-package lsp-python-ms
   :ensure t
+  :init (setq lsp-python-ms-auto-install-server t)
   :hook (python-mode . (lambda ()
-                         (require 'lsp-python-ms)
-                         (lsp))))
+  			 (require 'lsp-python-ms)
+  			 (lsp))))
 
 ;; (defun my-web-mode-hook ()
 ;;   (setq web-mode-markup-indent-offset 2)
@@ -416,7 +389,7 @@
 ;; )
 ;; (use-package web-mode
 ;;   :mode (("\\.html$" . web-mode)
-;;	 ("\\.ejs$" . web-mode))
+;; 	 ("\\.ejs$" . web-mode))
 ;;   :init
 ;;   (add-hook 'web-mode-hook  'my-web-mode-hook)
 ;;   )
@@ -427,14 +400,28 @@
 ;;   (add-hook 'web-mode-hook 'emmet-mode)
 ;;   )
 
+(use-package yaml-mode
+  :ensure t
+  :mode ("\\.yaml\\'" "\\.yml\\'")
+  :config
+  (setq yaml-indent-offset 4)
+  :custom-face
+  (font-lock-variable-name-face ((t (:foreground "violet")))))
 
+(use-package json-mode
+  :ensure t)
+
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 (use-package restclient
   :ensure t
-  :mode (("\\.http$" . restclient-mode))
-  )
-
-;; (setq restclient-inhibit-cookies t)
+  :mode (("\\.http$" . restclient-mode)))
 
 (use-package editorconfig
   :ensure t
@@ -451,14 +438,57 @@
   :init
   (setq drag-stuff-mode t)
   :config
-  (add-to-list 'drag-stuff-except-modes 'org-mode)
   (drag-stuff-define-keys))
+
+(set-face-attribute 'default nil :font "Fira Code" :height 100)
+
+;; (use-package doom-modeline
+;;   :ensure t
+;;   :custom
+;;   (doom-modeline-minor-modes nil)
+;;   :init (doom-modeline-mode 1))
+
+(use-package nyan-mode
+  :ensure t
+  :config
+  (nyan-mode 1)
+  (nyan-start-animation)
+  (nyan-toggle-wavy-trail)
+  :hook
+  (doom-modeline-mode . nyan-mode))
+
+;; (use-package doom-modeline
+;;   :ensure t
+;;   :custom
+;;   (doom-modeline-buffer-file-name-style 'truncate-with-project)
+;;   (doom-modeline-icon t)
+;;   (doom-modeline-major-mode-icon t)
+;;   (doom-modeline-minor-modes nil)
+;;   :init (doom-modeline-mode 1)
+;;   :config
+;;   (set-cursor-color "cyan"))
+
+(use-package dockerfile-mode
+  :ensure t)
 
 (use-package docker
   :ensure t
   :bind ("C-c d" . docker))
 
-(set-face-attribute 'default nil :font "Fira Code" :height 140)
+(use-package go-mode
+  :ensure t
+  :custom (gofmt-command "goimports")
+  :config
+  (add-hook 'before-save-hook #'gofmt-before-save)
+  (use-package gotest
+    :ensure t)
+  (use-package go-tag
+    :ensure t
+    :config (setq go-tag-args (list "-transform"))))
+
+(use-package elixir-mode
+  :ensure t)
+
 
 (when (window-system)
   (set-frame-font "Fira Code"))
@@ -491,5 +521,3 @@
   (dolist (char-regexp alist)
     (set-char-table-range composition-function-table (car char-regexp)
                           `([,(cdr char-regexp) 0 font-shape-gstring]))))
-
-
