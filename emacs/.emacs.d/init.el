e8a03faf92ace70617785e2d7dbd6ec1fe119e5b
Raiszo <E05.sek@gmail.com>
2026-07-29 20:45:03 -0500

    fix: follow symlinks option should be set in init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,2 +1,4 @@
 (package-initialize)
+;; need to set this so the emacs process follows the symlink (~/.emacs.d) to dotfiles
+(setq vc-follow-symlinks t)
 (org-babel-load-file "~/.emacs.d/configuration.org")
